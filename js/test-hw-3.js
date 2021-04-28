e0a5a4ac357edc512b11897aed40aafbdc221c98
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-04-28 17:20:07 +0200

    hw-3

---
+++ js/test-hw-3.js
@@ -0,0 +1,64 @@
+// z-1
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+// };
+
+
+// z-2
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Просторная квартира в центре',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+//    	owner: {
+//       name: 'Генри',
+//       phone: '982-126-1588',
+//       email: 'henry.carter@aptmail.com',
+//     },
+//   };
+
+
+// z-3
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Просторная квартира в центре',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+//   };
+  
+//   // Пиши код ниже этой строки
+//   const aptRating = apartment.rating;
+//   const aptDescr = apartment.descr;
+//   const aptPrice = apartment.price;
+//   const aptTags = apartment.tags;
+//   // Пиши код выше этой строки
+
+
+// z-4
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Генри',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com',
+//   },
+// };
+
+// // Пиши код ниже этой строки
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags[0];
+// const lastTag = apartment.tags[2];
+// // Пиши код выше этой строки
