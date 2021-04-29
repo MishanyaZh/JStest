8bbf6291a10e57714ff93e20fc0ee928da2341e1
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-04-30 01:26:33 +0200

    z-10

--- js/test-hw-3.js
+++ js/test-hw-3.js
@@ -62,3 +62,109 @@
 // const firstTag = apartment.tags[0];
 // const lastTag = apartment.tags[2];
 // // Пиши код выше этой строки
+
+
+// z-5
+// const apartment = {
+//     imgUrl: 'https://via.placeholder.com/640x480',
+//     descr: 'Просторная квартира в центре',
+//     rating: 4,
+//     price: 2153,
+//     tags: ['premium', 'promoted', 'top'],
+// };
+
+// // Пиши код ниже этой строки
+// const aptRating = apartment['rating'];
+// const aptDescr = apartment['descr'];
+// const aptPrice = apartment['price'];
+// const aptTags = apartment['tags'];
+// // Пиши код выше этой строки
+
+
+// z-6
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+//   tags: ['premium', 'promoted', 'top'],
+//   owner: {
+//     name: 'Генри',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com'
+//   }
+// };
+
+// // Пиши код ниже этой строки
+// apartment.price = 5000;
+// apartment.rating = 4.7;
+// apartment.owner.name = 'Генри Сибола';
+// apartment.tags.push('trusted');
+
+
+// z-7
+// const apartment = {
+//   imgUrl: 'https://via.placeholder.com/640x480',
+//   descr: 'Просторная квартира в центре',
+//   rating: 4.7,
+//   price: 5000,
+//   tags: ['premium', 'promoted', 'top', 'trusted'],
+//   owner: {
+//     name: 'Генри Сибола',
+//     phone: '982-126-1588',
+//     email: 'henry.carter@aptmail.com'
+//   }
+// };
+
+// // Пиши код ниже этой строки
+// apartment.area = 60;
+// apartment.rooms = 3;
+// apartment.location = {};
+// apartment.location.country = 'Ямайка';
+// apartment.location.city = 'Кингстон';
+
+
+// z-8
+// const name = 'Ремонтный дроид';
+// const price = 2500;
+// const image = 'https://via.placeholder.com/640x480';
+// const tags = ['on sale', 'trending', 'best buy'];
+
+// const product = {
+//   // Пиши код ниже этой строки
+//   name,
+//   price,
+//   image,
+//   tags
+//   // Пиши код выше этой строки
+// };
+
+
+// z-9
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//   // Пиши код ниже этой строки
+//   [emailInputName]:'henry.carter@aptmail.com',
+//   [passwordInputName]:'jqueryismyjam'
+//   // Пиши код выше этой строки
+// };
+
+
+// z-10
+// const apartment = {
+//   descr: 'Просторная квартира в центре',
+//   rating: 4,
+//   price: 2153,
+// };
+// const keys = [];
+// const values = [];
+// // Пиши код ниже этой строки
+// for (const key in apartment) {
+// keys.push(key);
+// }
+
+// for (const key in apartment) {
+// values.push(apartment[key]);
+// }
