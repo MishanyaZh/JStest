59e6265cf4107f447a0698773715522ddf2ad84b
MishanyaZh <76045588+MishanyaZh@users.noreply.github.com>
2021-05-20 17:53:26 +0200

    hw-6-23

--- js/test-hw-6.js
+++ js/test-hw-6.js
@@ -196,24 +196,27 @@
 // const oddNumbers = numbers.filter(number => number= number % 2!==0);
 
 
+
+const books = [
+    {
+      title: 'Последнее королевство',
+      author: 'Бернард Корнуэлл',
+      genres: ['приключения', 'историческое']
+    },
+    {
+      title: 'На берегу спокойных вод',
+      author: 'Роберт Шекли',
+      genres: ['фантастика', 'мистика']
+    },
+    {
+      title: 'Красна как кровь',
+      author: 'Ли Танит',
+      genres: ['ужасы', 'мистика', 'приключения']
+    }
+];
+
+
 // z-16
-// const books = [
-//     {
-//       title: 'Последнее королевство',
-//       author: 'Бернард Корнуэлл',
-//       genres: ['приключения', 'историческое']
-//     },
-//     {
-//       title: 'На берегу спокойных вод',
-//       author: 'Роберт Шекли',
-//       genres: ['фантастика', 'мистика']
-//     },
-//     {
-//       title: 'Красна как кровь',
-//       author: 'Ли Танит',
-//       genres: ['ужасы', 'мистика', 'приключения']
-//     }
-//   ];
 //   // Пиши код ниже этой строки
 //   const allGenres = books.flatMap(book=> book.genres);
 // console.table(allGenres);
@@ -251,80 +254,82 @@
 // // Пиши код выше этой строки
 
 
-// z-19
 
-// const users = [
-//   {
-//     name: 'Moore Hensley',
-//     email: 'moorehensley@indexia.com',
-//     eyeColor: 'blue',
-//     friends: ['Sharron Pace'],
-//     isActive: false,
-//     balance: 2811,
-//     gender: 'male',
-//     age: 37
-//   },
-//   {
-//     name: 'Sharlene Bush',
-//     email: 'sharlenebush@tubesys.com',
-//     eyeColor: 'blue',
-//     friends: ['Briana Decker', 'Sharron Pace'],
-//     isActive: true,
-//     balance: 3821,
-//     gender: 'female',
-//     age: 34
-//   },
-//   {
-//     name: 'Ross Vazquez',
-//     email: 'rossvazquez@xinware.com',
-//     eyeColor: 'green',
-//     friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
-//     isActive: false,
-//     balance: 3793,
-//     gender: 'male',
-//     age: 24
-//   },
-//   {
-//     name: 'Elma Head',
-//     email: 'elmahead@omatom.com',
-//     eyeColor: 'green',
-//     friends: ['Goldie Gentry', 'Aisha Tran'],
-//     isActive: true,
-//     balance: 2278,
-//     gender: 'female',
-//     age: 21
-//   },
-//   {
-//     name: 'Carey Barr',
-//     email: 'careybarr@nurali.com',
-//     eyeColor: 'blue',
-//     friends: ['Jordan Sampson', 'Eddie Strong'],
-//     isActive: true,
-//     balance: 3951,
-//     gender: 'male',
-//     age: 27
-//   },
-//   {
-//     name: 'Blackburn Dotson',
-//     email: 'blackburndotson@furnigeer.com',
-//     eyeColor: 'brown',
-//     friends: ['Jacklyn Lucas', 'Linda Chapman'],
-//     isActive: false,
-//     balance: 1498,
-//     gender: 'male',
-//     age: 38
-//   },
-//   {
-//     name: 'Sheree Anthony',
-//     email: 'shereeanthony@kog.com',
-//     eyeColor: 'brown',
-//     friends: ['Goldie Gentry', 'Briana Decker'],
-//     isActive: true,
-//     balance: 2764,
-//     gender: 'female',
-//     age: 39
-//   }
-// ]
+
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39
+  }
+]
+
+// z-19
 // // Пиши код ниже этой строки
 
 // const getUsersWithAge = (users, minAge, maxAge) => {
@@ -335,6 +340,60 @@
 // };
 // // console.table(getUsersWithAge(users,20,30));
 // // getUsersWithAge(users,30,40);
+// // Пиши код выше этой строки
+
+
+// z-20
+// // Пиши код ниже этой строки
+// const getUsersWithFriend = (users, friendName) => {
+//   const friend = users.filter(user =>
+//     user.friends.includes(friendName)
+//     );
+//     //2
+//      console.log(friend);
+    
+//     return friend;
+//     //1
+     
+// };
+// // Пиши код выше этой строки
+
+// //2
+//  getUsersWithFriend(users, 'Briana Decker')
+
+// //1
+// console.log(getUsersWithFriend(users, 'Briana Decker'));
+
+
+// z-21
+// // Пиши код ниже этой строки
+// const getFriends = (users) => {
+
+//     const allFriens = users.flatMap(user => user.friends);
+    
+//     const unicFriends = allFriens.filter((friend, index, arr) => arr.indexOf(friend) === index);
+    
+//   return unicFriends;
+// };
+// // Пиши код выше этой строки
+// console.log(getFriends(users));
+
+
+// z-22
+// // Пиши код ниже этой строки
+// const getActiveUsers = (users) => {
+//    const activ = users.filter(user=> user.isActive===true)
+//    return activ;
+// };
+// // Пиши код выше этой строки
+
+
+// z-23
+// // Пиши код ниже этой строки
+// const getInactiveUsers = (users) => {
+//   const activOff = users.filter(user=> user.isActive!==true)
+//     return activOff; 
+// };
 // // Пиши код выше этой строки
 
 
